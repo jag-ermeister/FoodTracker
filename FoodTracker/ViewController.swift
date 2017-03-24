135146b1903d8ef2008cf4a50b363defdcafdcc3
Michael Pace <mpace@pivotal.io>
2017-03-24 11:21:19 -0600

    Starting to work through the FoodTracker tutorial.

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by pivotal on 3/24/17.
+//  Copyright © 2017 Pivotal Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
